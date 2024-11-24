d5609d86e4dcfa5e8ae040ecbb180229bc93f833
Iago <iago2011.andrade@hotmail.com>
2024-11-24 11:26:11 -0300

    Finalização dia 15, interface de App música

--- day-15/MusicAppUI/app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ day-15/MusicAppUI/app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -2,6 +2,7 @@ package com.example.musicappui.ui.theme
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,6 +21,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionErrors
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,9 +30,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.musicappui.AccountDialog
+import com.example.musicappui.MainViewModel
 import com.example.musicappui.Screen
 import com.example.musicappui.screensInDrawer
 import kotlinx.coroutines.CoroutineScope
@@ -42,13 +50,19 @@ fun MainView() {
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+    val viewModel: MainViewModel = viewModel()
 
     // Allow us to find out on which "View" we current are
     val controller: NavController = rememberNavController()
     val navBackStackEntry by controller.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
-    val title = remember { mutableStateOf("") }
+    val dialogOpen = remember { mutableStateOf(false) }
+
+
+    val currentScreen = remember { viewModel.currentScreen.value }
+
+    val title = remember { mutableStateOf(currentScreen.title) }
 
 
     Scaffold(
@@ -78,7 +92,7 @@ fun MainView() {
                             scaffoldState.drawerState.close()
                         }
                         if (item.dRoute == "add_account") {
-                            // open dialog
+                            dialogOpen.value = true
                         } else {
                             controller.navigate(item.dRoute)
                             title.value = item.dTitle
@@ -89,7 +103,9 @@ fun MainView() {
             }
         }
     ) {
-        Text(text = "", modifier = Modifier.padding(it))
+        Navigation(navController = controller, viewModel = viewModel, pd = it)
+
+        AccountDialog(dialogOpen = dialogOpen)
     }
 }
 
@@ -127,5 +143,20 @@ fun DrawerItem(
 }
 
 
+@Composable
+fun Navigation(navController: NavController, viewModel: MainViewModel, pd: PaddingValues) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.DrawerScreen.Account.route,
+        modifier = Modifier.padding(pd)
+    ) {
 
+        composable(Screen.DrawerScreen.Account.route) {
+            AccountView()
+        }
 
+        composable(Screen.DrawerScreen.Subscription.route) {
+            SubscriptionView()
+        }
+    }
+}
